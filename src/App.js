e0b01c706ee6de15664becb6045daa4b470157ef
proiacm <proiacm@gmail.com>
2020-10-09 12:12:00 -0600

    Create Parent component folder

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 class App extends Component {
   // all React components must render!
   render() {
-    // return (
-    //   <div className="App">
-    //   {/* we will nest all other components here and only render the root App in ReactDOM render */}
-    //    <h1>Hi, I'm a React App!</h1>
-    //   </div>
-    // );
-   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Now I should have an h1 tag with text inside the div with css class styling')) // what's happening behind the scenes of the above code
+    return (
+      // everything here is JSX not HTML
+      <div className="App"> 
+      {/* we will nest all other components here and only render the root App in ReactDOM render */}
+       <h1>Hi, I'm a React App!</h1>
+       <p>I'm writing more elements</p>
+      </div>
+    );
+  //  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Now I should have an h1 tag with text inside the div with css class styling')) // what's happening behind the scenes of the above code
   }
 }
 
