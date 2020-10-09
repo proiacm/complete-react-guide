0bfe22584308ee8546bc745c585b1edbeb0903cc
proiacm <proiacm@gmail.com>
2020-10-09 15:30:32 -0600

    Create props for Person

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
       {/* we will nest all other components here and only render the root App in ReactDOM render */}
        <h1>Hi, I'm a React App!</h1>
        <p>I'm writing more elements in the div</p>
-       <Person />
+       <Person  name='Maria' age='29'>Job: Software Developer</Person>
+       <Person name='Ryan' age='34'/>
+       {/* If we inspect the element in the browser, the Person component is nothing more than a p tag within the main div */}
       </div>
     );
   //  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Now I should have an h1 tag with text inside the div with css class styling')) // what's happening behind the scenes of the above code
