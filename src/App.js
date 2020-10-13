9959cfd9348a4476d2c325d120b834a221f1a994
proiacm <proiacm@gmail.com>
2020-10-13 10:09:43 -0600

    Pass method as props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import './App.css';
 import Person from './Person/Person';
-import { Component } from 'react';
+
 
 class App extends Component {
    
@@ -20,20 +20,20 @@ class App extends Component {
       ]})
       }
       
-  render () {    
-    return (
-      // everything here is JSX not HTML
-      <div className="App"> 
-      {/* we will nest all other components here and only render the root App in ReactDOM render */}
-       <h1>Hi, I'm a React App!</h1>
-       <p>I'm writing more elements in the div</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person  name={this.state.people[0].name} age={this.state.people[0].age}>Job: Software Developer</Person>
-       <Person name={this.state.people[1].name} age={this.state.people[1].age}/>
-       {/* If we inspect the element in the browser, the Person component is nothing more than a p tag within the main div */}
-      </div>
-    );
-  }
+    render () {    
+      return (
+        // everything here is JSX not HTML
+        <div className="App"> 
+        {/* we will nest all other components here and only render the root App in ReactDOM render */}
+        <h1>Hi, I'm a React App!</h1>
+        <p>I'm writing more elements in the div</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person  name={this.state.people[0].name} age={this.state.people[0].age} changeName={this.switchNameHandler} >Job: Software Developer</Person>
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}/>
+        {/* If we inspect the element in the browser, the Person component is nothing more than a p tag within the main div */}
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,7 +6,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old</p>
+            <p onClick={props.changeName}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
         </div>
     )
