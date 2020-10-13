49eb7e5c7e75aae4e0d8f1287e1db43485db9042
proiacm <proiacm@gmail.com>
2020-10-13 10:38:17 -0600

    Play with CSS

--- src/App.js
+++ src/App.js
@@ -27,14 +27,23 @@ class App extends Component {
     ]})
     }  
       
-    render () {    
+    render () {   
+      
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      };
+
       return (
         // everything here is JSX not HTML
         <div className="App"> 
         {/* we will nest all other components here and only render the root App in ReactDOM render */}
         <h1>Hi, I'm a React App!</h1>
         <p>I'm writing more elements in the div</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button style={style} onClick={() => this.switchNameHandler('CiaraMaria')}>Switch Name</button>
         <Person  name={this.state.people[0].name} age={this.state.people[0].age} click={this.switchNameHandler.bind(this, 'CiaraMaria')}>Job: Software Developer</Person>
         <Person name={this.state.people[1].name} age={this.state.people[1].age} change={this.nameChangeHandler}/>
         {/* If we inspect the element in the browser, the Person component is nothing more than a p tag within the main div */}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Person.css'
 
 // props is merely an object with all of the attributes of the component
 // when using a class-based components, we use this.props
 //children refers to any elements between the opening and closing tag of the component 
 const person = (props) => {
     return (
-        <div>
+        <div className='Person'>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} /> 
