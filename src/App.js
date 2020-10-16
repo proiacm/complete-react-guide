b84a55843912a820e00c32910d0b4e62390fce55
proiacm <proiacm@gmail.com>
2020-10-16 08:57:41 -0600

    Add toggle

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
        people: [
         { name: 'Maria', age: '29'},
         { name: 'Ryan', age: '34'}
-      ]
+      ],
+      showPeople: false
     }
 
     switchNameHandler = (newName) => {
@@ -26,6 +27,11 @@ class App extends Component {
         { name: event.target.value , age: '34'}
     ]})
     }  
+
+    togglePeopleHandler = () => {
+      const doesShow =  this.state.showPeople;
+      this.setState({showPeople: !doesShow}) // if doesShow is true it sets to false, if doesShow is false it sets to true
+    }
       
     render () {   
       
@@ -40,13 +46,15 @@ class App extends Component {
       return (
         // everything here is JSX not HTML
         <div className="App"> 
-        {/* we will nest all other components here and only render the root App in ReactDOM render */}
-        <h1>Hi, I'm a React App!</h1>
-        <p>I'm writing more elements in the div</p>
-        <button style={style} onClick={() => this.switchNameHandler('CiaraMaria')}>Switch Name</button>
-        <Person  name={this.state.people[0].name} age={this.state.people[0].age} click={this.switchNameHandler.bind(this, 'CiaraMaria')}>Job: Software Developer</Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age} change={this.nameChangeHandler}/>
-        {/* If we inspect the element in the browser, the Person component is nothing more than a p tag within the main div */}
+          <h1>Hi, I'm a React App!</h1>
+          <p>I'm writing more elements in the div</p>
+          <button style={style} onClick={this.togglePeopleHandler}>Show Names</button>
+          { this.state.showPeople ? 
+            <div>
+            <Person  name={this.state.people[0].name} age={this.state.people[0].age} click={this.switchNameHandler.bind(this, 'CiaraMaria')}>Job: Software Developer</Person>
+            <Person name={this.state.people[1].name} age={this.state.people[1].age} change={this.nameChangeHandler}/>
+          </div> : null
+          }
         </div>
       );
     }
