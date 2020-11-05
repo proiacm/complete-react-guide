aaa60b9e8aa5925c9bf16ae875a64deea5e3b419
proiacm <proiacm@gmail.com>
2020-11-05 10:12:12 -0700

    add css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -49,19 +49,6 @@ class App extends Component {
     }
       
     render () {   
-      
-      const style = {
-        backgroundColor: 'green',
-        color: 'white',
-        font: 'inherit',
-        border: '1px solid blue',
-        padding: '8px',
-        cursor: 'pointer',
-        ':hover': {
-          backgroundColor: 'lightgreen',
-          color: 'black'
-        }
-      };
 
       let people = null;
 
@@ -73,26 +60,21 @@ class App extends Component {
             })}
           </div>
         );
-        style.backgroundColor = 'red';
-        style[':hover'] = {
-          backgroundColor: 'salmon',
-          color: 'black'
-        }
       }
 
-      let classes = [];
+      let assignedClasses = [];
       if (this.state.people.length <= 2){
-        classes.push('red'); // classes = ['red']
+        assignedClasses.push(classes.red); // assignedClasses = ['red']
       }
       if (this.state.people.length <= 1) {
-        classes.push('bold'); // classes = ['red', 'bold']
+        assignedClasses.push(classes.bold); // assignedClasses = ['red', 'bold']
       }
 
       return (
-          <div className="App"> 
+          <div className={classes.App}> 
             <h1>Hi, I'm a React App!</h1>
-            <p className={classes.join(' ')}>I will change based on length of list</p>
-            <button style={style} onClick={this.togglePeopleHandler}>Toggle People</button>
+            <p className={assignedClasses.join(' ')}>I will change based on length of list</p>
+            <button className={classes.Button} onClick={this.togglePeopleHandler}>Toggle People</button>
             {people}
           </div>
       );
