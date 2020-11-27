701e3584391e206771c818b332808ab88cd1bf37
proiacm <proiacm@gmail.com>
2020-11-27 12:10:39 -0700

    Add Aux hoc

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import classes from './Person.css'
+import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
 
 const person = (props) => {
 
     return (
-        <div className={classes.Person}>
+        <Aux>
+        {/* // <div className={classes.Person}> */}
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} /> 
-        </div>
+        {/* // </div> */}
+        </Aux>
     )
 }
 
